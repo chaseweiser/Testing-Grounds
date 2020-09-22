b60946b49047429f5c3bc90a911fcaae1689a476
Chase Weiser <ChaseWeiser@gmail.com>
2020-09-21 21:18:07 -0700

    Added loop function to uc.sh

--- uc.sh
+++ uc.sh
@@ -2,17 +2,39 @@
 # Append /Documents/useful-commands.txt with new commands
 
 
+user=$(whoami)
 # Defining the append function to clean the code up below
-function append_file {
-	echo $newcommand >> /home/chase/Documents/useful-commands.txt
+function APPEND_FILE {
+	echo $NEWCOMMAND >> /home/chase/Documents/useful-commands.txt
 }
 
-user=$(whoami)
+#until [[ $RESPONSE = "y" ]]; do 
+#	echo "What command would you like to add? " 
+#	read NEWCOMMAND 
+#	echo "$NEWCOMMAND - is that correct? (y/n)" 
+#	read RESPONSE
+#	if [[ $RESPONSE = "y" ]]; then
+#		APPEND_FILE
+#		echo "Thank you, $USER. $NEWCOMMAND has been added"
+#	fi
+#done
+#echo "Exiting now..."
 
-echo "What command would you like to add, $user?"
-read newcommand
-echo $newcommand '- correct? (y/n)'
-read response
+#[ $RESPONSE == 'y' ] && APPEND_FILE && echo "$NEWCOMMAND has been added" || echo "See you later" && exit 0
 
-# Eventually come back in and make this loop back if 'n' is entered
-[ $response == 'y' ] && append_file && echo "$newcommand has been added" || echo "Better luck next time" && exit
+
+until [[ $RESPONSE = "y" ]]; do
+	read -p "What command would you like to add? " NEWCOMMAND 
+	read -p "$NEWCOMMAND - is that correct? (y/n) " RESPONSE 
+	if [[ $RESPONSE = "y" ]]; then
+		APPEND_FILE
+		echo "Thank you, $USER. $NEWCOMMAND has been added"
+	fi
+done
+echo "Exiting now..."
+
+
+
+
+
+
