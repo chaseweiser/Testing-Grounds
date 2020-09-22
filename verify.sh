c1d7f44fdc44003138bc1145225b530afebb8904
Chase Weiser <ChaseWeiser@gmail.com>
2020-09-21 23:09:59 -0700

    Verify two inputs

--- verify.sh
+++ verify.sh
@@ -3,17 +3,24 @@
 # Simple Bash script to compare two hashes
 
 
-user=$(whoami)
-date=$(date +%A)
+USER=$(whoami)
+FILE=./.COUNTER_FILE
 
-echo "Hello $user. Please paste your hashes below to verify:"
+#Ensure COUNTER_FILE exists and create one if not
+if [ ! -f "$FILE" ]; then
+	touch .COUNTER_FILE
+fi
+
+
+echo "Hello $USER. Please paste your hashes below to verify:"
 read -p "Hash #1: " HASH1
 read -p "Hash #2: " HASH2
 
 [ $HASH1 ==  $HASH2 ] && echo "We're good to go cap!" || echo "We have a problemo"
 
-< counter_file read counter
-counter=$(( $counter + 1 ))
-echo $counter > counter_file
-echo "You have verified $counter hashes"
+< .COUNTER_FILE read COUNTER
+COUNTER=$(( $COUNTER + 1 ))
+echo $COUNTER > .COUNTER_FILE
+echo "You have compared $COUNTER hashes"
+
 
